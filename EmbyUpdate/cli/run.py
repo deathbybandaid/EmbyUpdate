f489fe0b592884267cb5a26ec18f36cb96deb6e1
deathbybandaid <github@deathbybandaid.net>
2021-09-10 11:24:08 -0400

    test

--- EmbyUpdate/cli/run.py
+++ EmbyUpdate/cli/run.py
@@ -91,13 +91,13 @@ def main(script_dir, deps):
         if args.setup:
             return config_setup(args, script_dir)  # TODO
 
-        print("Test ran")
-
         return start(args, script_dir, deps)
 
     except KeyboardInterrupt:
         print("\n\nInterrupted")
         return ERR_CODE
+
+    print("Test ran")
 
     return ERR_CODE
 
