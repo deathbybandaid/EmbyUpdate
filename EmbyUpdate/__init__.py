659d6b514339b060f7c35cbed8b60154b77e27c6
deathbybandaid <github@deathbybandaid.net>
2021-09-10 12:17:00 -0400

    test

--- EmbyUpdate/__init__.py
+++ EmbyUpdate/__init__.py
@@ -1,21 +1,7 @@
 # coding=utf-8
+import json
 
 EmbyUpdate_VERSION = "v3.2"
-
-
-class EmbyUpdate_INT_OBJ():
-
-    def __init__(self, settings, logger, versions, web, deps):
-        """
-        An internal catalogue of core methods.
-        """
-
-        self.version = EmbyUpdate_VERSION
-        self.versions = versions
-        self.config = settings
-        self.logger = logger
-        self.web = web
-        self.deps = deps
 
 
 class EmbyUpdate_OBJ():
@@ -25,13 +11,17 @@ class EmbyUpdate_OBJ():
         The Core Backend.
         """
 
-        logger.info("Initializing EmbyUpdate Core Functions.")
-        self.embyupdate = EmbyUpdate_INT_OBJ(settings, logger, versions, web, deps)
+        self.version = EmbyUpdate_VERSION
+        self.versions = versions
+        self.config = settings
+        self.logger = logger
+        self.web = web
+        self.deps = deps
 
-    def __getattr__(self, name):
-        """
-        Quick and dirty shortcuts. Will only get called for undefined attributes.
-        """
+        self.release_url = "https://api.github.com/repos/mediabrowser/Emby.releases/releases"
 
-        if hasattr(self.embyupdate, name):
-            return eval("self.embyupdate.%s" % name)
+    def update_check(self):
+
+        response = self.web.get(self.release_url)
+        updatejson = json.loads(response.text)
+        print(updatejson)
--- EmbyUpdate/cli/run.py
+++ EmbyUpdate/cli/run.py
@@ -19,7 +19,7 @@ def build_args_parser(script_dir):
     parser = argparse.ArgumentParser(description='EmbyUpdate')
     parser.add_argument('-c', '--config', dest='cfg', type=str, default=pathlib.Path(script_dir).joinpath('config.ini'), required=False, help='configuration file to load.')
     parser.add_argument('--setup', dest='setup', type=str, required=False, nargs='?', const=True, default=False, help='Setup Configuration file.')
-    parser.add_argument('-v', '--version', dest='version', type=str, required=False, nargs='?', const=True, default=False, help='Show Version Number.')
+    parser.add_argument('-v', '--version', dest='version', type=str, required=False, nargs='?', const=True, default=False, help='Displays Version Number.')
     return parser.parse_args()
 
 
@@ -29,13 +29,7 @@ def run(settings, logger, script_dir, versions, deps, web):
     """
 
     embyupdate = EmbyUpdate_OBJ(settings, logger, versions, web, deps)
-
-    try:
-
-        print("Doing Stuff Now")
-
-    except KeyboardInterrupt:
-        return ERR_CODE
+    embyupdate.update_check()
 
     return ERR_CODE
 
